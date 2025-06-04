711ad95208c2483ab7d453372f41dd60aaf8bf60
Smila3 <sag349@scarletmail.rutgers.edu>
2025-06-04 00:45:53 -0400

    Sushi, bot responds in server

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, ChannelType } = require('discord.js');
 
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
-const client = new Client({ intents: [
-	GatewayIntentBits.Guilds,
-	GatewayIntentBits.GuildMembers,
-	GatewayIntentBits.GuildMessages,
-	GatewayIntentBits.MessageContent
-] });
+const client = new Client({ 
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.DirectMessages
+
+	], 
+
+    partials: [
+		'CHANNEL', 'MESSAGE'
+	],});
 
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
@@ -16,11 +23,11 @@ client.once(Events.ClientReady, readyClient => {
 
 client.on(Events.MessageCreate, message => {
 
-	console.log(message); //can us message.content if you only want to get the message text
-	if (message.content.includes("sushi")){
-		message.reply("yes please! :3")
+	console.log(message); //can use message.content if you only want to get the message text
+	if (message.content.includes("sushi") && !message.author.bot) {
+		message.reply("yes please! I want sushi!")
 	}
-
+	
 });
 
 client.login(process.env.TOKEN);
