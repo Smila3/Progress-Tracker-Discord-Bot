864154fd252e20c0930f0833ae198490bf013b9a
Smila3 <sag349@scarletmail.rutgers.edu>
2025-06-03 20:41:03 -0400

    Updated Guilds

--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ const { Client, Events, GatewayIntentBits } = require('discord.js');
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMembers,
+	GatewayIntentBits.GuildMessages,
+	GatewayIntentBits.MessageContent
+] });
 
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.on(Events.MessageCreate, message => {
+
+	console.log(message); //can us message.content if you only want to get the message text
+	if (message.content.includes("sushi")){
+		message.reply("yes please! :3")
+	}
+
 });
 
 client.login(process.env.TOKEN);
