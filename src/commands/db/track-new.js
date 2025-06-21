ebbb1608b91ad488b8ce0b1e01aaa0a3142d6441
Smila3 <sag349@scarletmail.rutgers.edu>
2025-06-21 02:22:55 -0400

    24 hour limit in track new

--- src/commands/db/track-new.js
+++ src/commands/db/track-new.js
@@ -21,8 +21,27 @@ module.exports = {
     
     async execute(interaction) { 
         const userId = interaction.user.id;
-        const activity = interaction.options.getString('activity');
+        let activity = interaction.options.getString('activity');
+        //Normalizing activity string to avoid reiteration:
+        activity = activity.toLowerCase();
+        activity = activity.charAt(0).toUpperCase() + activity.slice(1);
+
         const hours = interaction.options.getInteger('hours');
+        const todayStr = new Date().toISOString().slice(0, 10);
+
+        //Ensure user is not entering more than 24 hours in activities today
+        const totalToday = db.prepare(`
+            SELECT SUM(hours) AS total 
+            FROM activity_logs 
+            WHERE user_id = ? AND DATE(timestamp) = ?
+        `).get(userId, todayStr)?.total || 0;
+
+        if (totalToday + hours > 24) {
+            await interaction.reply(`You’ve already logged ${totalToday}h today. You can't log more than 24 hours in a single day!`);
+            return;
+        }
+
+        
 
         // Check if user exists in the database
         const existing = db.prepare(`SELECT 1 FROM users WHERE user_id = ?`).get(userId);
